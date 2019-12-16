e478fc629d1f8f00b863aa2e319b850aa425bae6
Shirom Makkad <shirmak11@gmail.com>
2019-12-16 16:02:16 -0600

    Created foundation for Salvation-Army-Like Navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,15 +1,33 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
 import '../scss/Navbar.scss'
 import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 
 export class Navigation extends React.Component {
     render() {
         return (
-            <Navbar id="nav" expand="md" variant="dark" sticky="top">
-                <Container>
-                    <Navbar.Brand href="#">Navbar</Navbar.Brand>
-                </Container>
+            <Navbar id="nav" expand="md" variant="light" sticky="top" style={{padding: 0}}>
+                <div className="" style={{width: "100%"}}>
+                    <div className="d-flex align-content-center" style={{backgroundColor: "white", width: "100%", paddingTop: 8, paddingBottom: 8, paddingLeft: 16, paddingRight: 16}}>
+                        <Container>
+                            <Navbar.Brand className="mx-md-auto" href="#">Inspire A Girl</Navbar.Brand>
+                            <Navbar.Toggle className="ml-auto" aria-controls="responsive-navbar-nav" />
+                        </Container>
+                    </div>
+                    <Navbar.Collapse id="responsive-navbar-nav" style={{backgroundColor: "#7c6cb2", width: "100%"}}>
+                        <Container>
+                            <Nav className="mx-auto">
+                                <Nav.Item>
+                                    <a className="navbar-link" href="#">Test1</a>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <a className="navbar-link" href="#">Test2</a>
+                                </Nav.Item>
+                            </Nav>
+                        </Container>
+                    </Navbar.Collapse>
+                </div>
             </Navbar>
         )
     }
