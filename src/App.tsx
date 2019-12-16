d905b31a5712145bd2c304943bcdcef983b014b0
Shirom Makkad <shirmak11@gmail.com>
2019-12-15 20:41:18 -0600

    Set bootstrap theming priority and allowed for bootstrap variables to be used universally

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from "react-bootstrap/Button";
-import './App.css';
 import Navigation from "./components/Navigation";
+import './scss/App.scss';
 
 const App: React.FC = () => {
   return (
