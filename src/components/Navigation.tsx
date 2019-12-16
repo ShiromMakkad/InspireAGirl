3bae31a7b1a41ec06e359baf357109d83dcbe6a1
Shirom Makkad <shirmak11@gmail.com>
2019-12-15 23:17:47 -0600

    Switched to Lato Font

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import '../scss/Navbar.scss'
+import Navbar from "react-bootstrap/Navbar";
 
 export class Navigation extends React.Component {
     render() {
         return (
-            <div>
-                <p>Navigation</p>
-            </div>
+            <Navbar id="nav" expand="md" variant="dark" sticky="top">
+                <Container>
+                    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                </Container>
+            </Navbar>
         )
     }
 }
