db5c1d09a68724aa6aa8bb0f451a1218556a156a
Shirom Makkad <shirmak11@gmail.com>
2019-12-16 20:40:40 -0600

    Fixed banner text positioning in mobile

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,15 @@ const App: React.FC = () => {
     return (
         <div className="App">
             <Navigation/>
-            <div id="tint">
-                <img src={require('./assets/bannerImage.jpg')} id="img" alt="Indian Girl"/>
-            </div>
-            <div id="bannerText">
-                <h1>Call to Action</h1>
-                <h3>Subtitle</h3>
-                <Button size="lg">BIGGGGG Button</Button>
+            <div id="banner">
+                <div id="tint">
+                    <img src={require('./assets/bannerImage.jpg')} id="img" alt="Indian Girl"/>
+                </div>
+                <div id="bannerText">
+                    <h1>Call to Action</h1>
+                    <h3>Subtitle</h3>
+                    <Button size="lg">BIGGGGG Button</Button>
+                </div>
             </div>
         </div>
     );
