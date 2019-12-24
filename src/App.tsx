f687e10c01848a05b90f4ba1420f0eb55009c6d4
Shirom Makkad <shirmak11@gmail.com>
2019-12-23 21:23:24 -0600

    Added Functionality to Contact Submit Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Navigation from "./components/Navigation";
 import './scss/App.scss';
 import Button from "react-bootstrap/Button";
@@ -42,6 +42,17 @@ const App: React.FC = () => {
             window.removeEventListener('scroll', handleScroll);
         }
     }, []);
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [body, setBody] = useState('');
+
+    const formSubmit = function (e) {
+        window.location.href = "mailto:contact@inspireagirl.org?subject=" + subject + "&body=" + body + escape("\r\n\r\n") + "- " + firstName + " " + lastName;
+        e.preventDefault();
+    };
 
     return (
         <div className="App">
@@ -143,32 +154,37 @@ const App: React.FC = () => {
             <div id="contact">
                 <h2>Contact Us To Donate</h2>
                 <Container id="contactFormContainer">
-                    <Form id="contactForm" action="mailto:youraddr@domain.tld">
+                    <Form id="contactForm" onSubmit={formSubmit}>
                         <Form.Row>
                             <Form.Group as={Col} controlId="formFirstName">
                                 <Form.Label>First Name</Form.Label>
-                                <Form.Control placeholder="First Name"/>
+                                <Form.Control required placeholder="First Name" value={firstName}
+                                              onInput={e => setFirstName(e.target.value)}/>
                             </Form.Group>
 
                             <Form.Group as={Col} controlId="formLastName">
                                 <Form.Label>Last Name</Form.Label>
-                                <Form.Control placeholder="Last Name"/>
+                                <Form.Control required placeholder="Last Name" value={lastName}
+                                              onInput={e => setLastName(e.target.value)}/>
                             </Form.Group>
                         </Form.Row>
 
                         <Form.Group controlId="formEmail">
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" placeholder="Email Address"/>
+                            <Form.Control required type="email" placeholder="Email Address" value={email}
+                                          onInput={e => setEmail(e.target.value)}/>
                         </Form.Group>
 
                         <Form.Group controlId="formSubject">
                             <Form.Label>Subject</Form.Label>
-                            <Form.Control placeholder="Subject"/>
+                            <Form.Control required placeholder="Subject" value={subject}
+                                          onInput={e => setSubject(e.target.value)}/>
                         </Form.Group>
 
                         <Form.Group controlId="formMessage">
                             <Form.Label>Message</Form.Label>
-                            <Form.Control as="textarea" rows="7"/>
+                            <Form.Control required as="textarea" rows="7" value={body}
+                                          onInput={e => setBody(e.target.value)}/>
                         </Form.Group>
 
                         <Button id="contactSubmit" variant="primary" type="submit">Submit</Button>
